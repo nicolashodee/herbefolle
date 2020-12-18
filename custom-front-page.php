9fd8a8738fadc5892f58b0afad93a94eeb80aa77
Nicolas HODEE <accounts@nicolashodee.com>
2020-12-18 12:42:32 +0100

    initial commit

---
+++ custom-front-page.php
@@ -0,0 +1,30 @@
+<?php /* Template Name: MyCustomFrontPage */ 
+get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <img src="http://herbe-folle.local/wp-content/uploads/2020/12/IMG_20201209_123336678.jpg" alt="image1">
+    </div>
+    <div class="col-sm">
+      Image2
+    </div>
+    <div class="col-sm">
+      Image3
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+      Image4
+    </div>
+    <div class="col-sm">
+      Image5
+    </div>
+    <div class="col-sm">
+      Image6
+    </div>
+  </div>
+</div>
+
+<?php
+get_footer();
