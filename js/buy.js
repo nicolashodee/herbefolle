5432f5a4c7a6ff5a4375fc13ccb623892f411f68
Nicolas HODEE <accounts@nicolashodee.com>
2021-04-16 23:40:01 +0100

    getting closer

--- js/buy.js
+++ js/buy.js
@@ -1,10 +1,10 @@
 const titles = gsap.utils.toArray('.buy__left');
 titles.forEach(title => {
-  const anim = gsap.from(title, { ease: Power2. easeOut, y: 80, paused: true, });
+  const anim = gsap.from(title, { ease: Power2. easeOut, y: 80, opacity: 0, paused: true, delay: 1.5});
   
   ScrollTrigger.create({
     trigger: title,
-    start: "top 90%",
+    start: "top 80%",
     onEnter: () => anim.play()
   });
   
@@ -14,3 +14,21 @@ titles.forEach(title => {
     onLeaveBack: () => anim.reverse()
   });
 });
+
+const blocks = gsap.utils.toArray('.buy__section');
+blocks.forEach(block => {
+  const anim = gsap.from(block, { ease: Power2. easeOut, duration: 1, y: 50, opacity: 0, paused: true });
+  
+  ScrollTrigger.create({
+    trigger: block,
+    start: "top 80%",
+    onEnter: () => anim.play()
+  });
+  
+  ScrollTrigger.create({
+    trigger: block,
+    start: "top 90%",
+    onLeaveBack: () => anim.reverse()
+  });
+});
+
