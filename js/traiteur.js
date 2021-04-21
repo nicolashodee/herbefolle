22bc916241f94651c95d35bf715476000061edba
Nicolas HODEE <accounts@nicolashodee.com>
2021-04-21 12:23:39 +0100

    all pages finished

---
+++ js/traiteur.js
@@ -0,0 +1,6 @@
+// HOMEPAGE LOADING
+const tlTraiteur = gsap.timeline({ defaults: {ease: 'power3.out'} }); //initializing a timeline
+tlTraiteur.from("#traiteur", {x: "-145%", opacity: 0,  duration: 1});
+tlTraiteur.from(".traiteur-element", {x: "-15%", opacity: 0,  duration: 1, stagger: 0.5}, "-=1");
+tlTraiteur.from(".basket", {y: "100%", opacity: 0,  duration: 1});
+tlTraiteur.from(".traiteur-button", {x: "-15%", opacity: 0,  duration: 0.5}, );
