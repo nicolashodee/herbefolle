48d18f0317b0b07f23860259dd4d3e22dfe7f81c
Nicolas HODEE <accounts@nicolashodee.com>
2021-04-29 11:43:53 +0100

    page 404

---
+++ app/public/wp-content/themes/herbefolle/404.php
@@ -0,0 +1,31 @@
+
+
+<?php
+/**
+ * Template Name: Brunch
+*/
+
+?>
+
+  <!----------------X------------ WP HEADER SECTION --------------X----------------->
+  <?php get_header(); ?>  
+
+  <!----------------X------------ MAIN SITE SECTION --------------X----------------->
+  
+
+
+
+  <main class="container">
+    <div class="thisweek-bg"></div>
+    <section class="articles">
+      <article class="article__404">
+          <h2>Erreur 404 - Page non trouvée</h2>
+          <p></p>
+          <h5>Il semblerait que ce contenu se soit aventuré au milieu des herbes... </h5>
+      </article>
+    </section>
+  </main>
+  
+
+<!--------------X---------- WP FOOTER SECTION ----------X----------->
+<?php get_footer(); ?>
